5bc29e56a7caf7f9062e3a0938bdb73d6c07723d
mayaraviana <mayaraviana60@hotmail.com>
2022-01-04 19:18:18 -0300

    props name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 
-function App() {
+function App(props) {
   const [name, setName] = useState('');
 
   return (
     <div className="App">
       <input type="text" onChange={e => setName(e.target.value)}/>
       <p>Hello {name} </p>
+      <p>Hello {props.nome} </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App nome="mayara viana"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
