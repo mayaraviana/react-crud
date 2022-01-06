118364a26e93172ce63666de77b6a74603566e84
mayaraviana <mayaraviana60@hotmail.com>
2022-01-05 23:31:28 -0300

    add bootswatch css to main file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import 'bootswatch/dist/cerulean/bootstrap.min.css'; 
 import './index.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
