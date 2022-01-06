32549a5d508a069335c181ebbbde3a91e058b121
mayaraviana <mayaraviana60@hotmail.com>
2022-01-06 11:10:22 -0300

    add navbar component into app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, { useState } from 'react';
+import Navbar from './components/navbar';
 
 function App(props) {
-	const [name, setName] = useState('');
-
-	const criaComboBox = () => {
-		const opcoes = ['Fulano', 'Cicrano'];
-		const comboBoxOpcoes = opcoes.map((opcao) => <option>{opcao}</option>);
-
-		return <select>{comboBoxOpcoes}</select>;
-	};
-
-	const MeuComboBox = () => criaComboBox();
-
 	return (
-		<div className="App">
-			<input type="text" onChange={(e) => setName(e.target.value)} />
-			<p>Hello {name} </p>
-			<p>Hello {props.nome} </p>
-			<MeuComboBox />
-			<div className="d-grid gap-2">
-				<button className="btn btn-lg btn-primary" type="button">
-					Block button
-				</button>
-				<button className="btn btn-lg btn-primary" type="button">
-					Block button
-				</button>
-			</div>
-		</div>
+		<>
+			<Navbar />
+		</>
 	);
 }
 
