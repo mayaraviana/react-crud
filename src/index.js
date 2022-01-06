eb780f63de0209b9e5bcdfc82cbd63fd588a6261
mayaraviana <mayaraviana60@hotmail.com>
2022-01-06 10:50:11 -0300

    prettier on index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import 'bootswatch/dist/cerulean/bootstrap.min.css'; 
-import './index.css'
-
+import 'bootswatch/dist/cerulean/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App nome="mayara viana"/>
+    <App nome="mayara viana" />
   </React.StrictMode>,
   document.getElementById('root')
 );
