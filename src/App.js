9d68cdc0659523e584b87ed868efb38d044d896c
mayaraviana <mayaraviana60@hotmail.com>
2022-01-05 23:05:23 -0300

    add ComBoxComponent

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { useState } from 'react';
 
 function App(props) {
   const [name, setName] = useState('');
+  
+  const criaComboBox = () => {
+    const opcoes = [ "Fulano", "Cicrano" ]
+    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option>)
+    
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  const MeuComboBox  = () => criaComboBox()
 
   return (
     <div className="App">
       <input type="text" onChange={e => setName(e.target.value)}/>
       <p>Hello {name} </p>
       <p>Hello {props.nome} </p>
+      <MeuComboBox />
     </div>
   );
 }
