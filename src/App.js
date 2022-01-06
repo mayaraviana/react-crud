8013bd53043a95311c0b41f45a8b42366ae79602
mayaraviana <mayaraviana60@hotmail.com>
2022-01-06 10:50:22 -0300

    prettier on app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { useState } from 'react';
 
 function App(props) {
-  const [name, setName] = useState('');
-  
-  const criaComboBox = () => {
-    const opcoes = [ "Fulano", "Cicrano" ]
-    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option>)
-    
-    return (
-      <select>
-        {comboBoxOpcoes}
-      </select>
-    )
-  }
+	const [name, setName] = useState('');
 
-  const MeuComboBox  = () => criaComboBox()
+	const criaComboBox = () => {
+		const opcoes = ['Fulano', 'Cicrano'];
+		const comboBoxOpcoes = opcoes.map((opcao) => <option>{opcao}</option>);
 
-  return (
-    <div className="App">
-      <input type="text" onChange={e => setName(e.target.value)}/>
-      <p>Hello {name} </p>
-      <p>Hello {props.nome} </p>
-      <MeuComboBox />
-      <div className="d-grid gap-2">
-        <button className="btn btn-lg btn-primary" type="button">Block button</button>
-        <button className="btn btn-lg btn-primary" type="button">Block button</button>
-      </div>
-    </div>
-  );
+		return <select>{comboBoxOpcoes}</select>;
+	};
+
+	const MeuComboBox = () => criaComboBox();
+
+	return (
+		<div className="App">
+			<input type="text" onChange={(e) => setName(e.target.value)} />
+			<p>Hello {name} </p>
+			<p>Hello {props.nome} </p>
+			<MeuComboBox />
+			<div className="d-grid gap-2">
+				<button className="btn btn-lg btn-primary" type="button">
+					Block button
+				</button>
+				<button className="btn btn-lg btn-primary" type="button">
+					Block button
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
