c76566e0237599d1e6b260d5431359c62834b284
mayaraviana <mayaraviana60@hotmail.com>
2022-01-20 10:36:32 -0300

    changes

--- src/views/products/productregistration.js
+++ src/views/products/productregistration.js
@@ -1,111 +1,153 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Productregistration() {
-  const [name, setName] = useState('');
-  const [sku, setSku] = useState('');
-  const [description, setDescription] = useState('');
-  const [price, setPrice] = useState('');
-  const [provider, setProvider] = useState('');
+import ProdutoService from '../../services/product';
 
-  const state = {
-    name,
-    sku,
-    description,
-    price,
-    provider,
+const estadoInicial = {
+  nome: '',
+  sku: '',
+  descricao: '',
+  preco: 0,
+  fornecedor: '',
+  sucesso: false,
+};
+
+class CadastroProduto extends React.Component {
+  state = estadoInicial;
+
+  constructor() {
+    super();
+    this.service = new ProdutoService();
+  }
+
+  onChange = (event) => {
+    const valor = event.target.value;
+    const nomeDoCampo = event.target.name;
+    this.setState({ [nomeDoCampo]: valor });
   };
 
-  const onSubmit = (event) => {
-    console.log(state);
+  onSubmit = (event) => {
+    const produto = {
+      nome: this.state.nome,
+      sku: this.state.sku,
+      descricao: this.state.descricao,
+      preco: this.state.preco,
+      fornecedor: this.state.fornecedor,
+    };
+    this.service.salvar(produto);
+    this.setState({ sucesso: true });
+    console.log('Salvo com sucesso!');
   };
 
-  const cleanForm = (event) => {
-    setName('');
-    setSku('');
-    setDescription('');
-    setPrice('');
-    setProvider('');
+  limpaCampos = () => {
+    this.setState(estadoInicial);
   };
 
-  return (
-    <div className="card">
-      <div className="card-header">Cadastro de Produto</div>
-      <div className="card-body">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="form-group">
-              <label>Name *</label>
-              <input
-                type="text"
-                className="form-control"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
+  render() {
+    return (
+      <div className="card">
+        <div className="card-header">Cadastro de produto</div>
+        <div className="card-body">
+          {this.state.sucesso && (
+            <div class="alert alert-dismissible alert-success">
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="alert"
+              ></button>
+              <strong>Feito!</strong> Salvo com sucesso!
+            </div>
+          )}
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Nome: *</label>
+                <input
+                  type="text"
+                  name="nome"
+                  onChange={this.onChange}
+                  value={this.state.nome}
+                  className="form-control"
+                />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>SKU: *</label>
+                <input
+                  type="text"
+                  name="sku"
+                  onChange={this.onChange}
+                  value={this.state.sku}
+                  className="form-control"
+                />
+              </div>
             </div>
           </div>
-          <div className="col-md-6">
-            <div className="form-group">
-              <label>SKU *</label>
-              <input
-                type="text"
-                className="form-control"
-                value={sku}
-                onChange={(e) => setSku(e.target.value)}
-              />
+
+          <div className="row">
+            <div className="col-md-12">
+              <div className="form-group">
+                <label>Descrição:</label>
+                <textarea
+                  name="descricao"
+                  onChange={this.onChange}
+                  value={this.state.descricao}
+                  className="form-control"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <div className="form-group">
-              <label>Description:</label>
-              <textarea
-                className="form-control"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
+
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Preço: *</label>
+                <input
+                  value={this.state.preco}
+                  onChange={this.onChange}
+                  name="preco"
+                  type="text"
+                  className="form-control"
+                />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Fornecedor: *</label>
+                <input
+                  type="text"
+                  name="fornecedor"
+                  onChange={this.onChange}
+                  value={this.state.fornecedor}
+                  className="form-control"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6">
-            <div className="form-group">
-              <label>Price *</label>
-              <input
-                type="text"
-                className="form-control"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-              />
+
+          <div className="row">
+            <div className="col-md-1">
+              <button
+                type="submit"
+                className="btn btn-success"
+                onClick={this.onSubmit}
+              >
+                Salvar
+              </button>
             </div>
-          </div>
-          <div className="col-md-6">
-            <div className="form-group">
-              <label>Provider *</label>
-              <input
-                type="text"
-                className="form-control"
-                value={provider}
-                onChange={(e) => setProvider(e.target.value)}
-              />
+
+            <div className="col-md-1">
+              <button onClick={this.limpaCampos} className="btn btn-primary">
+                Limpar
+              </button>
             </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-1">
-            <button className="btn btn-success" onClick={onSubmit}>
-              Salvar
-            </button>
-          </div>
-          <div className="col-md-1">
-            <button className="btn btn-primary" onClick={cleanForm}>
-              Limpar
-            </button>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default Productregistration;
+export default CadastroProduto;
