a7fb24dbe714f12c8d1ea18e0bf0df037e6b6fca
mayaraviana <mayaraviana60@hotmail.com>
2022-01-20 09:16:46 -0300

    add product resgistration route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './views/home';
+import Productregistration from './views/products/productregistration';
 
 function routes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
+        <Route
+          exact
+          path="/product-registration"
+          element={<Productregistration />}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
