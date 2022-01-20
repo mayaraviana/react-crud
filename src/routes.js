c7d363fd1c4e493442edbd561be6a48d0780c132
mayaraviana <mayaraviana60@hotmail.com>
2022-01-20 13:41:26 -0300

    implement Links on page

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './views/home';
 import Productregistration from './views/products/productregistration';
+import ConsultaProdutos from './views/products/consulta';
 
 function routes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route
-          exact
-          path="/product-registration"
-          element={<Productregistration />}
-        ></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route
+        exact
+        path="/product-registration"
+        element={<Productregistration />}
+      ></Route>
+      <Route
+        exact
+        path="/consulta-produtos"
+        element={<ConsultaProdutos />}
+      ></Route>
+    </Routes>
   );
 }
 
